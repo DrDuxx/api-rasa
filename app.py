24817c6757d49f337799b664a0a80827cabd6c97
Elias Lichaa El Khoury <elias.lichaa@adreatorsmena.com>
2021-07-09 17:26:27 +0300

    m

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 from flask import Flask, jsonify, request
+from flask_socketio import SocketIO, send
 import random
 import requests
 import os
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        colors = ['red', 'blue', 'yellow']
-        rand = random.randrange(0, 3)
-        return jsonify({"color": colors[rand]})
-    else:
-        body = request.get_json()
-        res = requests.post(os.environ.get("API_URL") + '/webhooks/rest/webhook', json=body)
-        final = res.json()
-        return jsonify(final[0])
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'GET':
+#         colors = ['red', 'blue', 'yellow']
+#         rand = random.randrange(0, 3)
+#         return jsonify({"color": colors[rand]})
+#     else:
+#         body = request.get_json()
+#         res = requests.post(os.environ.get("API_URL") + '/webhooks/rest/webhook', json=body)
+#         final = res.json()
+#         return jsonify(final[0])
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    try:
+        msg
+    except NameError:
+        msg="hey"
+
+    data = {"sender":"User","message":msg}
+    res = requests.post(os.environ.get("API_URL") + '/webhooks/rest/webhook', json=data)
+    final = res.json()
+    send(final[0]["text"])
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
