c3b30bb06b67e0cf287a6c0d47741082683ce225
Elias Lichaa El Khoury <elias.lichaa@adreatorsmena.com>
2021-07-09 09:03:47 +0300

    m

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, request
 import random
+import requests
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if(request.method=='GET'):
-        colors = ['red', 'blue', 'green']
+        colors = ['red', 'blue', 'yellow']
         rand = random.randrange(0, 3)
         return jsonify({"color":colors[rand]})
     else:
         body = request.get_json()
+        # res = requests.post('/webhooks/rest/webhook')
         return jsonify(body)
 
 
