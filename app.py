eb949052cb0d1308eaa645bf9b581f3a43e3ee8e
Elias Lichaa El Khoury <elias.lichaa@adreatorsmena.com>
2021-07-09 12:00:13 +0300

    m

--- app.py
+++ app.py
@@ -8,18 +8,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if (request.method == 'GET'):
+    if request.method == 'GET':
         colors = ['red', 'blue', 'yellow']
         rand = random.randrange(0, 3)
         return jsonify({"color": colors[rand]})
     else:
         body = request.get_json()
-        if body['sender'] != '':
-            res = requests.post(os.environ.get("API_URL") + '/webhooks/rest/webhook', json=body)
-            final = res.json()
-            return jsonify(final[0])
-        else:
-            return {"color" : "green"}
+        res = requests.post(os.environ.get("API_URL") + '/webhooks/rest/webhook', json=body)
+        final = res.json()
+        return jsonify(final[0])
 
 
 if __name__ == '__main__':
